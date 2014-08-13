f16fcdd27b57713803b743a1515eaf9c6a21220d
Oscar Kraemer <carloscarkraemer@gmail.com>
2014-08-13 16:32:56 +0300

    Tier2Monitoring
    First commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+save the Tier2MonitorSite as source.html
+run the parse.py
+./parse.py > result.js
+
---
+++ parse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+worda = '</li> <li> <a href="'
+wordb = '" tar'
+wordc = worda + "http"
+wordNot = "http://dashb-ssb"
+print "var links =["
+with open("source.html") as file:
+	for line in file:
+		if (wordc in line) and (not wordNot in line) :
+			x = line.index(worda)
+                	y = line.index(wordb)
+			print '"' + line[x+len(worda):y] + '", '
+		else:
+			continue
+print "];"
